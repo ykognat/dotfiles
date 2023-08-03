dc5a83f59d0e1bf7a1d86a8789e0e82b605743b0
Peter Frnco <peter.frnco@protonmail.com>
2023-08-03 22:05:26 +0200

    Disabled ncmpcpp and newsboat

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,9 +1,9 @@
 sleep 4
 unclutter -idle 30 -noevents & fcitx5 -d & Discord & /usr/libexec/notification-daemon & dwmblocks & mpd & firefox & volctl & syncthing & nm-applet &
-st -e newsboat  &
-sleep 1
+#st -e newsboat  &
+#sleep 1
 st &
-sleep 1
-st -e ncmpcpp & 
+#sleep 1
+#st -e ncmpcpp & 
 sleep 5
 # redshift-gtk -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"')
