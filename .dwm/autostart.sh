bb09e3e59ac298eef5857a98ca1fb738014cca02
Peter Frnco <peter.frnco@protonmail.com>
2024-03-01 06:40:43 +0100

    Change autostart.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -3,4 +3,4 @@ pipewire & unclutter -idle 30 -noevents & fcitx5 -d & /usr/libexec/notification-
 sleep 1
 sxhkd &
 sleep 5
-/usr/local/bin/kanata --cfg /home/peter/.config/kanata/keymap.kbd &
+#/usr/local/bin/kanata --cfg /home/peter/.config/kanata/keymap.kbd &
