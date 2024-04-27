a5ae5d4fee10c6fa1f002f2c6a669c85f942dbe2
Peter Frnco <peter.frnco@protonmail.com>
2024-04-27 16:54:28 +0200

    Remove unneeded dotfiles

--- .dwm/autostart.sh
+++
@@ -1,6 +0,0 @@
-sleep 4
-pipewire & unclutter -idle 30 -noevents & fcitx5 -d & /usr/libexec/notification-daemon & dwmblocks & firefox & syncthing & nm-applet & alacritty & blueman-applet & Discord & volctl & mpd & 
-sleep 1
-sxhkd &
-sleep 5
-#/usr/local/bin/kanata --cfg /home/peter/.config/kanata/keymap.kbd &
