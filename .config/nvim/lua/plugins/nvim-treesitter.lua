41aa54810e895390d557a2e755e450300fa0d2c3
Peter Frnco <peter.frnco@protonmail.com>
2024-05-03 02:36:20 +0200

    Add json to parsers

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -6,11 +6,14 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "rust", "sql", "nix", "bash", "python", "norg" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "rust", "sql", "nix", "bash", "python", "norg", "json", "jsonc", "css" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
         })
+      -- local parsers = require "nvim-treesitter.parsers"
+      -- local parser_config = parsers.get_parser_configs()
+      -- parser_config.jsonc.filetype_to_parsername = "json"
     end
  }
 }
