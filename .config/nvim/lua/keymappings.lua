866d8eaa50ecd9fd8465afd238d0fce480cceded
Peter Frnco <peter.frnco@protonmail.com>
2024-05-07 23:17:09 +0200

    Change tab width to 4

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -9,32 +9,32 @@ vim.api.nvim_set_keymap('n', '<leader>tf', ':Telescope find_files<CR>', { norema
 vim.api.nvim_create_user_command(
     "SaveAs",
     function(opts)
-      SaveAsUser(opts.args)
+        SaveAsUser(opts.args)
     end,
     {
-      nargs = 1,
-      desc = "Save the current file as a specified user"
+        nargs = 1,
+        desc = "Save the current file as a specified user"
     }
 )
 vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function()
-    local bufmap = function(mode, lhs, rhs)
-      local opts = {buffer = true}
-      vim.keymap.set(mode, lhs, rhs, opts)
-    end
+    desc = 'LSP actions',
+    callback = function()
+        local bufmap = function(mode, lhs, rhs)
+            local opts = {buffer = true}
+            vim.keymap.set(mode, lhs, rhs, opts)
+        end
 
-    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-    bufmap('i', '<c-m-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
-    bufmap('n', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
-    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-    -- bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-    -- bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-    -- bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-  end})
+        bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+        bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+        bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+        bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+        bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+        bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+        bufmap('i', '<c-m-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+        bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+        bufmap('n', '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
+        bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+        -- bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+        -- bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+        -- bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+    end})
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -8,9 +8,9 @@ o.relativenumber = true -- Show line numbers relative to the cursor's position
 
 g.python_recommended_style = 0
 g.rust_recommended_style = 0
-opt.tabstop = 2
+opt.tabstop = 4
 opt.smartindent = true
-opt.shiftwidth = 2
+opt.shiftwidth = 4
 opt.expandtab = true
 
 -- Enable line wrapping
