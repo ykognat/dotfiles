b87170cf9324804856ca6eaf498833b11ed2d826
Peter Frnco <peter.frnco@protonmail.com>
2024-06-04 16:51:00 +0200

    nvim oil adjustments

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -1,11 +1,6 @@
 vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tb', ':Telescope buffers<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tf', ':Telescope find_files<CR>', { noremap = true, silent = true })
-
--- vim.api.nvim_set_keymap('n', '<c-w>-', ':vertical resize -5<CR>', { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<c-w>=', ':vertical resize +5<CR>', { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<c-w>,', ':resize -5<CR>', { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<c-w>.', ':resize +5<CR>', { noremap = true, silent = true })
 vim.api.nvim_create_user_command(
     "SaveAs",
     function(opts)
--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,10 +1,22 @@
 return {
   'stevearc/oil.nvim',
   opts = {},
-  -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("oil").setup()
+    local oil = require("oil")
+    local util = require("oil.util")
+
+    -- Keymap to open Oil with preview, handling already open case
+    vim.keymap.set('n', '<s-tab>', function()
+      oil.open()
+    end, { desc = "Open Oil" })
+
+    -- Setup Oil plugin with any configuration options
+    oil.setup({
+      keymaps = {
+        ["<tab>"] = "actions.select",
+      },
+    })
   end
 }
 
